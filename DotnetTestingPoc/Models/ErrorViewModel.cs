2a5c7eabdd1ad7d87e2e181aa16f65fa87cf24af
Now <now.thongtangsai@gmail.com>
2024-10-02 19:04:38 +0700

    BE: init the .net proj and install efcore

---
+++ DotnetTestingPoc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetTestingPoc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
